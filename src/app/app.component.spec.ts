f47810c66bb374f45860454259d7eeaf1363f0ea
ChrisTarasovs <chris@frontr.co.uk>
2019-04-09 14:09:00 +0100

    'test'

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { HmctsrolesLibModule } from 'hmctsroles-lib';
+import {HmctsrolesLibModule} from '../../projects/hmctsroles-lib/src/lib/hmctsroles-lib.module';
+
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
